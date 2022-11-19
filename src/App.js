d5156d1ce275e2845e21a344ebe411fe755433d0
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-11-19 19:57:19 +0100

    did the same for quizes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import QuestionsFetch from './services/fetching/questions';
+import QuizesFetch from './services/fetching/quizes';
 
 
 function App() {
   return (
     <div className="App">
-      { <QuestionsFetch type="multiple" yearFrom="1917" yearTo="1990" tags="II wojna swiatowa"/> }
-      
+      {/* { <QuestionsFetch type="multiple" yearFrom="1917" yearTo="1990" tags="II wojna swiatowa"/> } */}
+      { <QuizesFetch /> }
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/services/fetching/quizes.js
+++ src/services/fetching/quizes.js
@@ -1,23 +1,40 @@
 import React from 'react';
+import FetchController from './main'; 
 
 class QuizesFetch extends React.Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
 
         this.getMultipleQuiz = this.getMultipleQuiz.bind(this);
         this.getSingleQuiz = this.getSingleQuiz.bind(this);
+
+        // init, the one who rules them all.
+        this.handleRequest(props);
+    }
+
+    handleRequest(){
+        const { type } = this.props;
+
+        if ( !type || type === 'single' ) {
+            this.getSingleQuiz(this.props);
+        } else {
+            this.getMultipleQuiz(this.props);
+        }
     }
 
     saveQuiz( accountId, questionsId, quizName, quizLevel ) {
-        
+        const fetch = new FetchController();
+        console.log(fetch.makeRequest('quiz/5', 'post', {accountId, questionsId, quizName, quizLevel}));
     }
 
     getMultipleQuiz( yearFrom, yearTo, level, amount ){
-
+        const fetch = new FetchController();
+        console.log(fetch.makeRequest('quiz/5,6,7,8'));
     }
     
     getSingleQuiz( yearFrom, yearTo, level ){
-    
+        const fetch = new FetchController();
+        console.log(fetch.makeRequest('quiz/5'));
     }
 
     render(){
