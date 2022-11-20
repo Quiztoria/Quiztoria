f6eeab01234bf8c300b8e57f0dfc599e9ce3cd78
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-11-20 01:40:12 +0100

    const replaced with class

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import QuestionsFetch from './services/fetching/questions';
 import QuizesFetch from './services/fetching/quizes';
+import React from 'react';
 
+class App extends React.Component{
+  constructor(){
+    super()
+  }
 
-function App() {
-  const forButtons = new QuestionsFetch({disableHandler:true});
-  const questionPostRequest = {            
+  forButtons = new QuestionsFetch({disableHandler:true});
+  questionPostRequest = {            
     "answCorrect": "tak",
     "answOpt1": "nie",
     "answOpt2": "nie",
@@ -15,36 +19,39 @@ function App() {
     "dateStart": parseInt(2022),
     "questionString": "test" 
   }
-  const questionDeleteRequest = { id: 1 };
-  const questionGetAllRequest    = { itemsPerPage: 10, page:0 };
-  const questionGetRequest    = { id: 1 };
+  questionDeleteRequest = { id: 1 };
+  questionGetAllRequest    = { itemsPerPage: 10, page:0 };
+  questionGetRequest    = { id: 1 };
 
-  return (
-    <div className="App">
-      { <QuestionsFetch id="1" type="single" yearFrom="1917" yearTo="1990" tags="II wojna swiatowa" /> }
-      {/* { <QuizesFetch type="save" accountId="1" questionsId="1,2,3" quizName="test" quizLevel="average"/> } */}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a onClick={() => forButtons.getAllQuestions(questionGetAllRequest)} >
-          QUESTION - GET ALL REQUEST
-        </a>
-        <a onClick={() => forButtons.getSingleQuestion(questionGetRequest)} >
-          QUESTION - GET SINGLE REQUEST
-        </a>
-        <a onClick={() => forButtons.saveQuestion(questionPostRequest)} >
-          QUESTION - POST REQUEST
-        </a>
-        <a onClick={() => forButtons.deleteQuestion(questionDeleteRequest)} >
-          QUESTION - DELETE REQUEST
-        </a>
+  render(){
+    return (
+      <div className="App">
+        { <QuestionsFetch id="1" type="single" yearFrom="1917" yearTo="1990" tags="II wojna swiatowa" /> }
+        {/* { <QuizesFetch type="save" accountId="1" questionsId="1,2,3" quizName="test" quizLevel="average"/> } */}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a onClick={() => this.forButtons.getAllQuestions(this.questionGetAllRequest)} >
+            QUESTION - GET ALL REQUEST
+          </a>
+          <a onClick={() => this.forButtons.getSingleQuestion(this.questionGetRequest)} >
+            QUESTION - GET SINGLE REQUEST
+          </a>
+          <a onClick={() => this.forButtons.saveQuestion(this.questionPostRequest)} >
+            QUESTION - POST REQUEST
+          </a>
+          <a onClick={() => this.forButtons.deleteQuestion(this.questionDeleteRequest)} >
+            QUESTION - DELETE REQUEST
+          </a>
+  
+          <hr style={{width:"300px"}}/>
+        </header>
+      </div>
+    );
+  }
 
-        <hr style={{width:"300px"}}/>
-      </header>
-    </div>
-  );
 }
 
 export default App;
